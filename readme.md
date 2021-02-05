b96b8e520ba45ce36d07182909d5b70b32bf939e
Santiago Rincon <santiagorincon2001@gmail.com>
2021-02-05 12:38:27 -0500

    Se agrega readme con instrucciones

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Ejemplos de importacion - exportacion con ES6 y Require
+- ES6
+- Require
+
+## ¿Como correr?
+
+Ejecuta:
+- `npm install` para instalar los paquetes
+- `npm build` para transpilar con webpack
+
+Abre el archivo index.html y en la consola podras ver todas las salidas.
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
+let MODE = "production";
+let WATCH = false;
+if(process.env.ENVIRONMENT === "development"){
+   MODE = "development";
+   WATCH = true;
+}
 module.exports = {
     entry: './src/index.js', // el punto de arranque de nuestro programa
     output: {
       path: __dirname + '/dist', // el path absoluto para el directorio donde queremos que el output sea colocado
       filename: 'main.js' // el nombre del archivo que va a contener nuestro output - podemos nombrarlo como queramos pero bundle.js es lo típico
     },
-    mode: 'development',
-    watch: true,
+    mode: MODE,
+    watch: WATCH,
   }
